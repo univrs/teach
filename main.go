390f9fb6571188831b3136bb42cc6c88e625a814
ardeshir sepahsalar <ardeshir.org@gmail.com>
2016-10-16 20:35:56 +0000

    adding new func

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import "fmt"
 func main() {
 
         // foo := &myStruct{"bar"}
-        foo := new(myStruct)
+        foo := newMyStruct()
 
-	// foo := myStruct{"bar"}
-        foo.myField = "bar"
+	    // foo := myStruct{"bar"}
+        foo.myField = "foo"
+        
+        foo.myMap["bar"] = "baz"
 
         fmt.Println(foo.myField)
+        fmt.Println(foo.myMap)
 }
 
 type myStruct struct {
+     myMap map[string]string
      myField string
 }
 
+func newMyStruct() *myStruct {
+    
+    result := myStruct{}
+    result.myMap = map[string]string{}
+    
+    return &result
+}
